4d0e060073d29e15924c4379af6b6c862df3b864
user0x01 <34656620+user0x01@users.noreply.github.com>
2022-08-31 16:13:16 +0200

    Add files via upload

--- gridmeter.py
+++ gridmeter.py
@@ -3,23 +3,22 @@
 """
 /data/Pathtothisscript/vedbus.py
 /data/Pathtothisscript/ve_utils.py
-python -m ensurepip --upgrade
-pip install paho-mqtt
+opkg update && opkg upgrade python3-pip
+pip3 install paho-mqtt
 """
-
 ##########################################
-servicename= "com.victronenergy.grid.sdm";
-customname = "Powermeter via MQTT" ## name in Venus
+servicename= 'com.victronenergy.grid.sdm';
+customname = 'Powermeter via MQTT' ## name in Venus
 deviceinstance = 183 ## vrm nummer
 ##########################################
 path_UpdateIndex = '/UpdateIndex';
-null_timeout = 5
+null_timeout = 3
 # MQTT
-mqttbroker_address = "Homeassistant.local"  # mqtt server
-mqttclientid = "Venus.GridMeter.1"
-mqttusername = "Venus"
-mqttpassword = ""
-topic = "venus/powermeter/values"
+mqttbroker_address = 'Homeassistant.local'  # mqtt server
+mqttclientid = 'Venus.GridMeter.1'
+mqttusername = 'Venus'
+mqttpassword = ''
+topic = 'venus/powermeter/values'
 ##########################################
 
 import os
@@ -45,37 +44,32 @@ except:
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '../ext/velib_python'))
 from vedbus import VeDbusService
 
-
 def on_disconnect(client, userdata, rc):
-    print("Client Got Disconnected")
+    print('Client Got Disconnected')
     if rc != 0:
-        print("Unexpected MQTT disconnection. Will auto-reconnect")
-
+        print('Unexpected MQTT disconnection. Will auto-reconnect')
     else:
-        print("rc value:" + str(rc))
-
+        print('rc value:' + str(rc))
     try:
-        print("Trying to Reconnect")
+        print('Trying to Reconnect')
         client.connect(mqttbroker_address)
     except Exception as e:
-        logging.exception("Fehler beim reconnecten mit Broker")
-        print("Error in Retrying to Connect with Broker")
+        logging.exception('Fehler beim reconnecten mit Broker')
+        print('Error in Retrying to Connect with Broker')
         print(e)
-
 
 def on_connect(client, userdata, flags, rc):
        if rc == 0:
-            print("Connected to MQTT Broker!")
+            print('Connected to MQTT Broker!')
             client.subscribe(topic)
        else:
-            print("Failed to connect, return code %d\n", rc)
+            print('Failed to connect, return code %d\n', rc)
 
 def on_message(client, userdata, msg):
     try:
         global meter
         jsonpayload = json.loads(msg.payload)
         meter._update(jsonpayload)
-
     except Exception as e:
         logging.exception(e)
         print(e)
@@ -85,9 +79,9 @@ class DbusService:
 
   def __init__(self, servicename, deviceinstance, pathsReadOnly, pathsReadWrite):
     self._dbusservice = VeDbusService(servicename)
-    gobject.timeout_add(1000, self._disconnect)
+    gobject.timeout_add(333, self._disconnect)
 
-    logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
+    logging.debug('%s /DeviceInstance = %d' % (servicename, deviceinstance))
 
     for path, settings in pathsReadOnly.items():
         self._dbusservice.add_path(path, settings['initial']);
@@ -112,27 +106,25 @@ class DbusService:
 
   def _update(self, jsonpayload):
     self.timestamp = datetime.datetime.now(datetime.timezone.utc)
-
     self._dbusservice['/Connected'] = 1
-    self._dbusservice['/Ac/Energy/Forward'] = float(jsonpayload["Forward"] or 0)
-    self._dbusservice['/Ac/Energy/Reverse'] = float(jsonpayload["Reverse"] or 0)
-    self._dbusservice['/Ac/Power'] = float(jsonpayload["Power"] or 0)
-    self._dbusservice['/Ac/L1/Voltage'] = float(jsonpayload["L1Voltage"] or 0)
-    self._dbusservice['/Ac/L2/Voltage'] = float(jsonpayload["L2Voltage"] or 0)
-    self._dbusservice['/Ac/L3/Voltage'] = float(jsonpayload["L3Voltage"] or 0)
-
-    self._dbusservice['/Ac/L1/Current'] = float(jsonpayload["L1Current"] or 0)
-    self._dbusservice['/Ac/L2/Current'] = float(jsonpayload["L2Current"] or 0)
-    self._dbusservice['/Ac/L3/Current'] = float(jsonpayload["L3Current"] or 0)
-    self._dbusservice['/Ac/L1/Power'] = float(jsonpayload["L1Power"] or 0)
-    self._dbusservice['/Ac/L2/Power'] = float(jsonpayload["L2Power"] or 0)
-    self._dbusservice['/Ac/L3/Power'] = float(jsonpayload["L3Power"] or 0)
-    self._dbusservice['/Ac/L1/Energy/Forward'] = float(jsonpayload["L1EnergyForward"] or 0)
-    self._dbusservice['/Ac/L1/Energy/Reverse'] = float(jsonpayload["L1EnergyReverse"] or 0)
-    self._dbusservice['/Ac/L2/Energy/Forward'] = float(jsonpayload["L2EnergyForward"] or 0)
-    self._dbusservice['/Ac/L2/Energy/Reverse'] = float(jsonpayload["L2EnergyReverse"] or 0)
-    self._dbusservice['/Ac/L3/Energy/Forward'] = float(jsonpayload["L3EnergyForward"] or 0)
-    self._dbusservice['/Ac/L3/Energy/Reverse'] = float(jsonpayload["L3EnergyReverse"] or 0)
+    self._dbusservice['/Ac/Energy/Forward'] = float(jsonpayload['Forward'] or 0)
+    self._dbusservice['/Ac/Energy/Reverse'] = float(jsonpayload['Reverse'] or 0)
+    self._dbusservice['/Ac/Power'] = float(jsonpayload['Power'] or 0)
+    self._dbusservice['/Ac/L1/Voltage'] = float(jsonpayload['L1Voltage'] or 0)
+    self._dbusservice['/Ac/L2/Voltage'] = float(jsonpayload['L2Voltage'] or 0)
+    self._dbusservice['/Ac/L3/Voltage'] = float(jsonpayload['L3Voltage'] or 0)
+    self._dbusservice['/Ac/L1/Current'] = float(jsonpayload['L1Current'] or 0)
+    self._dbusservice['/Ac/L2/Current'] = float(jsonpayload['L2Current'] or 0)
+    self._dbusservice['/Ac/L3/Current'] = float(jsonpayload['L3Current'] or 0)
+    self._dbusservice['/Ac/L1/Power'] = float(jsonpayload['L1Power'] or 0)
+    self._dbusservice['/Ac/L2/Power'] = float(jsonpayload['L2Power'] or 0)
+    self._dbusservice['/Ac/L3/Power'] = float(jsonpayload['L3Power'] or 0)
+    self._dbusservice['/Ac/L1/Energy/Forward'] = float(jsonpayload['L1EnergyForward'] or 0)
+    self._dbusservice['/Ac/L1/Energy/Reverse'] = float(jsonpayload['L1EnergyReverse'] or 0)
+    self._dbusservice['/Ac/L2/Energy/Forward'] = float(jsonpayload['L2EnergyForward'] or 0)
+    self._dbusservice['/Ac/L2/Energy/Reverse'] = float(jsonpayload['L2EnergyReverse'] or 0)
+    self._dbusservice['/Ac/L3/Energy/Forward'] = float(jsonpayload['L3EnergyForward'] or 0)
+    self._dbusservice['/Ac/L3/Energy/Reverse'] = float(jsonpayload['L3EnergyReverse'] or 0)
     self._dbusservice[path_UpdateIndex] = (self._dbusservice[path_UpdateIndex] + 1) % 256;
     return True
 
@@ -164,27 +156,27 @@ def main():
       '/Connected': {'initial': 1}
     },
     pathsReadWrite={
-      '/Ac/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}kWh".format(v)},
-      '/Ac/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}kWh".format(v)},
-      '/Ac/Power': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L1/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}V".format(v) },
-      '/Ac/L2/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}V".format(v) },
-      '/Ac/L3/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}V".format(v) },
-      '/Ac/L1/Current': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.2f}A".format(v) },
-      '/Ac/L2/Current': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.2f}A".format(v) },
-      '/Ac/L3/Current': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.2f}A".format(v) },
-      '/Ac/L1/Power': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L2/Power': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L3/Power': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L1/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v)},
-      '/Ac/L1/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L2/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L2/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v)},
-      '/Ac/L3/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v) },
-      '/Ac/L3/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: "{:0.1f}W".format(v)},
+      '/Ac/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}kWh'.format(v)},
+      '/Ac/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}kWh'.format(v)},
+      '/Ac/Power': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L1/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}V'.format(v) },
+      '/Ac/L2/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}V'.format(v) },
+      '/Ac/L3/Voltage': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}V'.format(v) },
+      '/Ac/L1/Current': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.2f}A'.format(v) },
+      '/Ac/L2/Current': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.2f}A'.format(v) },
+      '/Ac/L3/Current': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.2f}A'.format(v) },
+      '/Ac/L1/Power': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L2/Power': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L3/Power': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L1/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v)},
+      '/Ac/L1/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L2/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L2/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v)},
+      '/Ac/L3/Energy/Forward': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v) },
+      '/Ac/L3/Energy/Reverse': {'initial': 0, 'gettextcallback': lambda p, v: '{:0.1f}W'.format(v)},
+      '/ErrorCode': {'initial': 0, 'gettextcallback': None},
       path_UpdateIndex: {'initial': 0, 'gettextcallback': None }
     })
-  
 
   client = mqtt.Client(mqttclientid)
   client.on_disconnect = on_disconnect
@@ -199,5 +191,5 @@ def main():
   mainloop = gobject.MainLoop()
   mainloop.run()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
